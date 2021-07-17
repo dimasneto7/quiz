b224968a8ecb1f9a9d1d652a41a78eaabc9f6803
Dimas Neto <dimas_fcn@outlook.com>
2021-07-16 21:37:56 -0300

    api

---
+++ pages/api/bancoDeQuestoes.ts
@@ -0,0 +1,39 @@
+import QuestaoModel from "../../model/questao";
+import RespostaModel from "../../model/resposta";
+
+const questoes: QuestaoModel[] = [
+  new QuestaoModel(306, "Qũal bicho transmite a Doençs de Chagas?", [
+    RespostaModel.errada("Abelha"),
+    RespostaModel.errada("Barata"),
+    RespostaModel.errada("Pulga"),
+    RespostaModel.certa("Barbeiro"),
+  ]),
+  new QuestaoModel(
+    202,
+    "Qũal fruto é conhecido no Norte e Nordeeste como 'jerimum'?",
+    [
+      RespostaModel.errada("Caju"),
+      RespostaModel.errada("Côco"),
+      RespostaModel.errada("Chuchu"),
+      RespostaModel.certa("Abóbora"),
+    ]
+  ),
+  new QuestaoModel(203, "Qũal é o coletibo de cães?", [
+    RespostaModel.errada("Manada"),
+    RespostaModel.errada("Alcateia"),
+    RespostaModel.errada("Rebanho"),
+    RespostaModel.certa("Mantilha"),
+  ]),
+  new QuestaoModel(
+    204,
+    "Qũal é o triângulo que tem todos os lados diferentes?",
+    [
+      RespostaModel.errada("Equilátero"),
+      RespostaModel.errada("Isóceles"),
+      RespostaModel.errada("Trapézio"),
+      RespostaModel.certa("Escaleno"),
+    ]
+  ),
+];
+
+export default questoes;
