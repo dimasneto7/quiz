f02cd37b345c905d116495f0357a877736d0b964
Dimas Neto <dimas_fcn@outlook.com>
2021-07-17 23:03:55 -0300

    temporizador

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,24 +1,38 @@
-import Questao from "../components/Questao";
+import React, { useState } from "react";
+import Questionario from "../components/Questionario";
 import QuestaoModel from "../model/questao";
 import RespostaModel from "../model/resposta";
 
+const questaoMock = new QuestaoModel(1, "Qual é a melhor cor?", [
+  RespostaModel.errada("Verde"),
+  RespostaModel.errada("Vermelha"),
+  RespostaModel.errada("Azul"),
+  RespostaModel.certa("Preta"),
+]);
+
 export default function Home() {
-  const questaoTeste = new QuestaoModel(1, "Melhor cor?", [
-    RespostaModel.errada("Verde"),
-    RespostaModel.errada("Vermelha"),
-    RespostaModel.errada("Azul"),
-    RespostaModel.errada("Preta"),
-  ]);
+  const [questao, setQuestao] = useState(questaoMock);
+
+  function questaoRespondida(questao: QuestaoModel) {}
+
+  function irPraProximoPasso() {}
+
   return (
     <div
       style={{
         display: "flex",
+        flexDirection: "column",
         justifyContent: "center",
         alignItems: "center",
         height: "100vh",
       }}
     >
-      <Questao valor={questaoTeste} />
+      <Questionario
+        questao={questao}
+        ultima={true}
+        questaoRespondida={questaoRespondida}
+        irPraProximoPasso={irPraProximoPasso}
+      />
     </div>
   );
 }
